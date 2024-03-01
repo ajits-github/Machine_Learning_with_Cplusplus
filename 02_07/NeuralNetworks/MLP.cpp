3cd0a7250dcc9aaf434f8096b9071831e8517ffb
Ajit <ajit110920@gmail.com>
2024-03-01 20:51:27 +0100

    02_07 cpp1

--- 02_07/NeuralNetworks/MLP.cpp
+++ 02_07/NeuralNetworks/MLP.cpp
@@ -12,4 +12,19 @@ Perceptron::Perceptron(int inputs, double bias){
 	generate(weights.begin(),weights.end(),frand);
 }
 
+// Run the perceptron. x is a vector with the input values.
+double Perceptron::run(vector<double> x){
+	x.push_back(bias);
+	double sum = inner_product(x.begin(),x.end(),weights.begin(),(double)0.0);
+	return sigmoid(sum);
+}
 
+// Set the weights. w_init is a vector with the weights.
+void Perceptron::set_weights(vector<double> w_init){
+	weights = w_init;
+}
+
+// Evaluate the sigmoid function for the floating point input x.
+double Perceptron::sigmoid(double x){
+	return 1.0/(1.0 + exp(-x));
+}
